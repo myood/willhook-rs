aa9214b2314ba70988bce11a3db5c4b83124ac98
myood <myoodster@gmail.com>
2023-02-04 13:22:42 +0100

    Add more keyboard integration tests

---
+++ tests/keyboard_injected.rs
@@ -0,0 +1,139 @@
+//! These tests inject programmatically keyboard events and verify if willhook's hook receive them properly.
+//! It is important to keep in mind, that these injected events are going through Windows OS.
+//! These injected events will appear in the context of the currently focused window.
+//! Hence, it is best to invoke below tests in one-time CLI environment.
+//! You may observe it by letters appearing in CLI input after test run finishes, like so:
+//! PS C:\workspace\willhook> cfgklaa
+
+#[cfg(test)]
+mod keyboard_hook_tests {
+    use willhook::*;
+    use willhook::hook::event::InputEvent;
+    use willhook::hook::event::KeyPress;
+    use willhook::hook::event::KeyboardEvent;
+    use willhook::hook::event::InputEvent::*;
+    use willhook::hook::event::KeyPress::*;
+    use willhook::hook::event::KeyboardKey;
+    use willhook::hook::event::KeyboardKey::*;
+    use willhook::hook::event::IsKeyboardEventInjected::*;
+    use mki::Keyboard;
+
+    fn a_key(key: KeyboardKey, press: KeyPress) -> Result<InputEvent, std::sync::mpsc::TryRecvError> {
+        Ok(Keyboard(KeyboardEvent {
+                        pressed: press,
+                        key: Some(key),
+                        is_injected: Some(Injected)}))
+    }
+
+    #[test]
+    fn press_one_keyboard_key() {
+        Keyboard::A.press();
+
+        let h = keyboard_hook().unwrap();
+        assert!(h.try_recv().is_err());
+
+        Keyboard::A.press();
+
+        assert_eq!(h.try_recv(), a_key(A, Down(false)));
+        assert!(h.try_recv().is_err());
+    }    
+    
+    #[test]
+    fn release_one_keyboard_key() {
+        Keyboard::B.release();
+    
+        let h = keyboard_hook().unwrap();
+        assert!(h.try_recv().is_err());
+
+        Keyboard::B.release();
+
+        assert_eq!(h.try_recv(), a_key(B, Up(false)));
+        assert!(h.try_recv().is_err());
+    }
+    
+    #[test]
+    fn click_one_keyboard_key() {
+        let h = keyboard_hook().unwrap();
+        assert!(h.try_recv().is_err());
+
+        Keyboard::C.click();
+
+        assert_eq!(h.try_recv(), a_key(C, Down(false)));
+        assert_eq!(h.try_recv(), a_key(C, Up(false)));
+        assert!(h.try_recv().is_err());
+    }
+
+    #[test]
+    fn click_one_system_key() {
+        let h = keyboard_hook().unwrap();
+        assert!(h.try_recv().is_err());
+
+        Keyboard::LeftAlt.press();
+        Keyboard::D.click();
+        Keyboard::LeftAlt.release();
+
+        assert_eq!(h.try_recv(), a_key(LeftAlt, Down(true)));
+        assert_eq!(h.try_recv(), a_key(D, Down(true)));
+        assert_eq!(h.try_recv(), a_key(D, Up(true)));
+        assert_eq!(h.try_recv(), a_key(LeftAlt, Up(false)));
+        assert!(h.try_recv().is_err());
+    }
+
+    #[test]
+    fn click_one_system_key_receive_interleaving() {
+        let h = keyboard_hook().unwrap();
+        assert!(h.try_recv().is_err());
+
+        Keyboard::LeftAlt.press();
+        assert_eq!(h.try_recv(), a_key(LeftAlt, Down(true)));
+        assert!(h.try_recv().is_err());
+
+        Keyboard::E.press();
+        Keyboard::E.release();
+        assert_eq!(h.try_recv(), a_key(E, Down(true)));
+        assert_eq!(h.try_recv(), a_key(E, Up(true)));
+        assert!(h.try_recv().is_err());
+
+        Keyboard::LeftAlt.release();
+        assert_eq!(h.try_recv(), a_key(LeftAlt, Up(false)));
+        assert!(h.try_recv().is_err());
+
+        assert!(h.try_recv().is_err());
+    }
+
+    #[test]
+    fn multiple_keys() {        
+        Keyboard::F.press();
+        Keyboard::G.click();
+        let h = keyboard_hook().unwrap();
+        assert!(h.try_recv().is_err());
+
+        Keyboard::LeftAlt.press();
+        assert_eq!(h.try_recv(), a_key(LeftAlt, Down(true)));
+        assert!(h.try_recv().is_err());
+
+        Keyboard::H.press();
+        Keyboard::I.click();
+        Keyboard::H.release();
+        assert_eq!(h.try_recv(), a_key(H, Down(true)));
+        assert_eq!(h.try_recv(), a_key(I, Down(true)));
+        assert_eq!(h.try_recv(), a_key(I, Up(true)));
+        assert_eq!(h.try_recv(), a_key(H, Up(true)));
+        assert!(h.try_recv().is_err());
+
+        Keyboard::LeftAlt.release();
+        assert_eq!(h.try_recv(), a_key(LeftAlt, Up(false)));
+        assert!(h.try_recv().is_err());
+        
+        Keyboard::J.release();
+        Keyboard::K.click();
+        Keyboard::L.click();        
+        assert_eq!(h.try_recv(), a_key(J, Up(false)));
+        assert_eq!(h.try_recv(), a_key(K, Down(false)));
+        assert_eq!(h.try_recv(), a_key(K, Up(false)));
+        assert_eq!(h.try_recv(), a_key(L, Down(false)));
+        assert_eq!(h.try_recv(), a_key(L, Up(false)));
+        assert!(h.try_recv().is_err());
+    }
+
+}
